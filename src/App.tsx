4a2a444c4c76617ed8bf76d2708c415054bd2441
janmatzek <jan.matzek@gmail.com>
2026-02-25 22:23:45 +0100

    limit piece selection and rotate board according to player color

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { BoardComponent } from "./ui/Board";
 import { initializeBoard, randomizedBoard } from "./game/initializeBoard";
 import { useState } from "react";
-import type { Square } from "./types";
+import type { Square, Board, Color } from "./types";
 
 function App() {
-  const [board, setBoard] = useState(randomizedBoard());
+  const [board, setBoard] = useState<Board>(initializeBoard());
+  const [boardHistory, setBoardHistory] = useState([board]);
   const [selectedSqaure, setSelectedSquare] = useState<Square | null>(null);
+  const [playerColor, setPlayerColor] = useState<Color>("white");
+
   return (
     <>
       <div className="w-screen h-screen bg-gray-800 flex justify-center items-center">
         <div>
           <BoardComponent
+            playerColor={playerColor}
             board={board}
             setBoard={setBoard}
+            boardHistory={boardHistory}
+            setBoardHistory={setBoardHistory}
             selectedSquare={selectedSqaure}
             setSelectedSquare={setSelectedSquare}
           ></BoardComponent>
--- src/ui/Board.tsx
+++ src/ui/Board.tsx
@@ -1,28 +1,71 @@
-import type { Square, Board } from "../types";
+import type { Square, Board, Color } from "../types";
 import { SquareComponent } from "./Square";
 import type { SquareProps } from "./Square";
 import React, { useMemo } from "react";
 import { getLegalMoves } from "../game/getLegalMoves";
 
 type BoardProps = {
+  playerColor: Color;
   board: Board;
   setBoard: React.Dispatch<React.SetStateAction<Board>>;
+  boardHistory: Board[];
+  setBoardHistory: React.Dispatch<React.SetStateAction<Board[]>>;
   selectedSquare: Square | null;
   setSelectedSquare: React.Dispatch<React.SetStateAction<Square | null>>;
 };
 
+function RowLabels({ labels }: { labels: number[] }) {
+  return (
+    <div className="grid grid-cols-1 w-fig">
+      {[...labels].reverse().map((label) => {
+        return (
+          <div className="w-8 h-16 flex justify-center items-center text-white">
+            {label}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function ColumnLabels({ labels }: { labels: string[] }) {
+  return (
+    <div className="pl-8 grid grid-cols-10 w-fig">
+      {labels.map((label) => {
+        return (
+          <div className="w-16 h-8 flex justify-center items-center text-white">
+            {label}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
 // TODO: handle `check` game state
-// TODO: store previous board state -> for en passant and rolling back illegal move
-// TODO: set player color - allow control over those pieces only
+// TODO: board history could also be used to visualize previous turns. It could also include the LLM reasoning
+// TODO: UI for setting player color
 export function BoardComponent(boardProps: BoardProps) {
+  let columnLabels = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  let rowLabels = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  let boardToRender = [...boardProps.board].reverse();
+  if (boardProps.playerColor === "black") {
+    columnLabels = [...columnLabels].reverse();
+    rowLabels = [...rowLabels].reverse();
+    boardToRender = boardProps.board.map((row) => {
+      return [...row].reverse();
+    });
+  }
+
   const legalMoves = useMemo(() => {
     return getLegalMoves(boardProps.board, boardProps.selectedSquare);
   }, [boardProps.selectedSquare]);
 
-  function selectSquare(squareProps: SquareProps) {
+  function selectSquare(squareProps: SquareProps): void {
     if (squareProps.isSelected) {
       boardProps.setSelectedSquare(null);
-    } else {
+    } else if (squareProps.square.piece?.color === boardProps.playerColor) {
       boardProps.setSelectedSquare(squareProps.square);
     }
   }
@@ -51,33 +94,40 @@ export function BoardComponent(boardProps: BoardProps) {
     // Set state of the new board, unselect origin square
     boardProps.setBoard(newBoard);
     boardProps.setSelectedSquare(null);
+
+    // Append the new state to Board History
+    boardProps.setBoardHistory([...boardProps.boardHistory, newBoard]);
   }
 
   return (
     <>
-      <div className="grid grid-cols-8 w-fit">
-        {[...boardProps.board].reverse().map((row, rowIndex) =>
-          row.map((square, colIndex) => {
-            const squareId = `${square.position.row}-${square.position.col}`;
-            const isLegal = legalMoves.includes(squareId);
-            const onClickAction = isLegal ? movePiece : selectSquare;
-            return (
-              <SquareComponent
-                square={square}
-                isSelected={
-                  boardProps.selectedSquare?.position.row ===
-                    square.position.row &&
-                  boardProps.selectedSquare?.position.col ===
-                    square.position.col
-                }
-                isLegal={isLegal}
-                onClickAction={onClickAction}
-                key={`${rowIndex}-${colIndex}`}
-              ></SquareComponent>
-            );
-          }),
-        )}
+      <div className="flex">
+        <RowLabels labels={rowLabels}></RowLabels>
+        <div className="grid grid-cols-8 w-fit">
+          {boardToRender.map((row, rowIndex) =>
+            row.map((square, colIndex) => {
+              const squareId = `${square.position.row}-${square.position.col}`;
+              const isLegal = legalMoves.includes(squareId);
+              const onClickAction = isLegal ? movePiece : selectSquare;
+              return (
+                <SquareComponent
+                  square={square}
+                  isSelected={
+                    boardProps.selectedSquare?.position.row ===
+                      square.position.row &&
+                    boardProps.selectedSquare?.position.col ===
+                      square.position.col
+                  }
+                  isLegal={isLegal}
+                  onClickAction={onClickAction}
+                  key={`${rowIndex}-${colIndex}`}
+                ></SquareComponent>
+              );
+            }),
+          )}
+        </div>
       </div>
+      <ColumnLabels labels={columnLabels}></ColumnLabels>
     </>
   );
 }
